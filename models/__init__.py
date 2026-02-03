78072fe3a1d4070233d468d6b286e027b2952cf4
Kenny <Fuyu0507161@outlook.com>
2026-02-03 16:48:52 +0800

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 # Models package
 from .visual_encoder import VisualEncoder
 from .language_encoder import LanguageEncoder
-from .projection import ProjectionHead
+from .projection import ProjectionHead, DualProjectionHead
 from .lane_decoder import LaneDecoder
 from .fusion_head import FusionHead
 from .consistency_loss import InfoNCELoss, CosineConsistencyLoss
@@ -10,6 +10,7 @@ __all__ = [
     "VisualEncoder",
     "LanguageEncoder",
     "ProjectionHead",
+    "DualProjectionHead",
     "LaneDecoder",
     "FusionHead",
     "InfoNCELoss",
